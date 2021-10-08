aa1c608b9abe7adb19c34bd07853d6fbcf8d733a
Nickolay <zylex77@gmail.com>
2021-10-09 00:33:43 +0300

    solve TwoSum

---
+++ src/test/java/com/panyukovnn/leetcode/TwoSum.java
@@ -0,0 +1,31 @@
+package com.panyukovnn.leetcode;
+
+import com.panyukovnn.leetcode.util.Util;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    @Test
+    public void test() {
+        int[] result = twoSum(new int[]{3, 2, 4}, 6);
+
+        Util.printArray(result);
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> expectedNums = new HashMap<>();
+
+        for (int i = 1; i < nums.length; i++) {
+            if (expectedNums.get(nums[i]) != null) {
+                return new int[]{expectedNums.get(nums[i]), i};
+            }
+
+            expectedNums.put(target - nums[i], i);
+        }
+
+        return null;
+    }
+}
